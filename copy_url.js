ed12cb1688192154630f2eb21d27355b881e1d26
rauschma <axel@rauschma.de>
2013-01-16 10:46:58 +0100

    Different way of formatting tweets.

--- copy_url.js
+++ copy_url.js
@@ -1,38 +1,55 @@
-// Print link:
-// clear ; echo -n "javascript:"; java -jar ~/shared/local/yuicompressor-*.jar copy_url.js ; echo
+// Copy bookmarklet URL to clipboard (--no-copyright => remove leading comments)
+// (echo -n "javascript:"; uglifyjs --no-copyright copy_url.js ; echo) | pbcopy
 
 (function() {
     // Change to following to your liking
     var hostEntries = [
-        [ "www.2ality.com", { htag: "#2ality" } ],
+        [ "www.2ality.com", {
+            func: function (data) {
+                data.ttitle = '“'+data.ttitle+'” by @rauschma #2ality';
+                data.thref = data.thref+" ^ar"
+            }
+          }
+        ],
         [ "addyosmani.com", { twit: "@addyosmani" } ],
         [ "arstechnica.com", { twit: "@arstechnica" } ],
-        [ "badassjs.com", { re: /^(.*) - Badass JavaScript$/, twit: "@badass_js" } ],
-        [ "blog.chromium.org", { re: /^Chromium Blog: (.*)$/, twit: "@ChromiumDev" } ],
-        [ "blog.nodejs.org", { re: /^(.*) « node blog$/, twit: "@nodejs" } ],
-        [ "dailyjs.com", { re: /^DailyJS: (.*)$/, twit: "@dailyjs" } ],
-        [ "daringfireball.net", { re: /^Daring Fireball Linked List: (.*)$/, twit: "@daringfireball" } ],
+        [ "badassjs.com", { twit: "@badass_js" } ],
+        [ "blog.chromium.org", { twit: "@ChromiumDev" } ],
+        [ "blog.mozilla.com", { twit: "@firefox" } ],
+        [ "blog.nodejs.org", { twit: "@nodejs" } ],
+        [ "dailyjs.com", { twit: "@dailyjs" } ],
+        [ "daringfireball.net", { twit: "@daringfireball" } ],
         [ "developer.palm.com", { twit: "@hpnews" } ],
-        [ "functionsource.com", { re: /^FunctionSource: (.*)$/, twit: "@functionsource" } ],
-        [ "hacks.mozilla.org", { re: /^(.*) ✩ Mozilla Hacks – the Web developer blog$/, twit: "@mozhacks" } ],
-        [ "www.html5rocks.com", { re: /^HTML5 Rocks - (.*)$/, twit: "@ChromiumDev" } ],
-        [ "www.macrumors.com", { re: /^(.*) - Mac Rumors$/, twit: "@MacRumors" } ],
-        [ "www.marco.org", { re: /^(.*) – Marco.org$/, twit: "@marcoarment" } ],
+        [ "functionsource.com", { twit: "@functionsource" } ],
+        [ "hacks.mozilla.org", { twit: "@mozhacks" } ],
+        [ "www.html5rocks.com", { twit: "@ChromiumDev" } ],
+        [ "www.macrumors.com", { twit: "@MacRumors" } ],
+        [ "www.marco.org", { twit: "@marcoarment" } ],
         [ "mashable.com", { twit: "@mashable" } ],
-        [ "www.nczonline.net", { re: /^(.*) \| NCZOnline$/, twit: "@slicknet" } ],
-        [ /^.*nytimes.com$/, { re: /^(.*) - NYTimes.com$/, twit: "@nytimes" } ],
-        [ "spinoff.comicbookresources.com", { re: /^(.*) « Spinoff Online – TV, Film and Entertainment News Daily$/, twit: "@SpinoffOnline" } ],
-        [ "www.theverge.com", { re: /^(.*) [|] The Verge$/, twit: "@verge" } ],
-        [ "unscriptable.com", { re: /^(.*) [|] Unscriptable.com$/, twit: "@unscriptable" } ],
+        [ "www.nczonline.net", { twit: "@slicknet" } ],
+        [ /^.*nytimes.com$/, { twit: "@nytimes" } ],
+        [ "www.remotesynthesis.com", { twit: "@remotesynth" } ],
+        [ /^.*smashingmagazine.com$/, { twit: "@smashingmag" } ],
+        [ "spinoff.comicbookresources.com", { twit: "@SpinoffOnline" } ],
+        [ "tagneto.blogspot.ca", { twit: "@jrburke" } ],
+        [ "thenextweb.com", { twit: "@TheNextWeb" } ],
+        [ "www.theverge.com", { twit: "@verge" } ],
+        [ "unscriptable.com", { twit: "@unscriptable" } ],
     ];
-    
+
     //----- Step 1: collect the input from the current page
     
-    var sel = (window.getSelection ? ""+window.getSelection() : "")
-          .replace("<", "&lt;"); // even in textarea, we have to escape <
-    var ttl = document.title;
-    var twit = "";
-    var htag = "";
+    var data = {};
+    var sel = (window.getSelection ? String(window.getSelection()) : "");
+
+    // HTML
+    data.htitle = document.title;
+    data.hhref = document.location.href;
+    
+    // Plain text, Twitter
+    data.ttitle = document.title;
+    data.thref = document.location.href;
+
     var hostDesc;
     hostEntries.some(function(hostEntry) {
         var key = hostEntry[0];
@@ -49,49 +66,56 @@
         }
     });
     if (hostDesc) {
-        if (hostDesc.re) {
-            var match = hostDesc.re.exec(ttl);
-            if (match) {
-                ttl = match[1];
-            } else {
-                alert("Warning: The title “"+ttl+"” does not match regex "+hostDesc.re);
+        if (hostDesc.func) {
+            hostDesc.func(data);
+        } else {
+            if (hostDesc.twit) {
+                data.ttitle = '“'+data.ttitle+'” by '+hostDesc.twit;
             }
         }
-        if (hostDesc.twit) {
-            twit = hostDesc.twit + " ";
-        }
-        if (hostDesc.htag) {
-            htag = " " + hostDesc.htag;
-        }
     }
-    var a={
-        ttl: ttl,
-        twit: twit,
-        href: document.location.href,
-        htag: htag,
-        selt: (sel ? "\n\n"+sel : ""), // plain text separator + selection
-        selh: (sel ? "<br>"+sel : "")  // HTML separator + selection
-    };
 
     //----- Step 2: write the output to a new page
-    
-    var d=window.open().document;
-    // Poor man's templating
-    d.write(
-        ( '<html><head><title>Copy link: {ttl}</title></head><body>'
-        // First rendered HTML for WYSIWYG editors (a single chunk for easy copying)
-        + '<a href="{href}">{ttl}</a>{selh}<br><br>'
-        // Then plain text: ...
+
+    var htmlProlog = tmpl(data,
+        '<html><head><title>Copy link: {htitle}</title></head><body>'
         + '<textarea id="text" cols="80" rows="10">'
-        // ...Twitter
-        + '{ttl} {twit}{href}{htag}'
-        // ...HTML source
-        + '\n\n&lt;a href="{href}"&gt;{ttl}&lt;/a&gt;'
-        // ...selection
-        + '{selt}'
-        + '</textarea>'
-        + '</body></html>'
-        ).replace(/{([a-z]+)}/g, function(g0,g1){return a[g1]})
     );
-    d.close(); // finish writing the HTML
+    // Plain text/Twitter
+    var text = tmpl(data,
+        '{ttitle}\n{thref}'
+        + '\n\n'
+    );
+    // HTML link
+    var selectionStart = text.length;
+    text += tmpl(data,
+        '<a href="{hhref}">{htitle}</a>'
+    );
+    var selectionEnd = text.length;
+    // Optionally: selection
+    if (sel) {
+        text += "\n\n" + sel;
+    }
+    var htmlEpilog = tmpl(data,
+        '</textarea>'
+        + '</body></html>'
+    );
+
+    var doc = window.open().document;
+    doc.write(htmlProlog + htmlEscape(text) + htmlEpilog);
+    doc.close(); // finish writing the HTML
+
+    var area = doc.getElementById("text");
+    area.focus();
+    area.selectionStart = selectionStart;
+    area.selectionEnd = selectionEnd;
+
+    function htmlEscape(text) {
+        return text.replace("<", "&lt;");
+    }
+
+    // Poor man's templating
+    function tmpl(data, text) {
+        return text.replace(/{([a-z]+)}/g, function(g0,g1){return data[g1]});
+    }
 }())
